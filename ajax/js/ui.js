c447141c70062964580d6f0a564c8eca1d80ded9
tree4852 <tree4852@naver.com>
2024-06-24 18:20:16 +0900

    css로 scroll 이벤트 커스텀 하기

--- ajax/js/ui.js
+++ ajax/js/ui.js
@@ -2,10 +2,10 @@ $(function(){
     $(window).on('scroll', function (){ //this개념조심하기
         let scrpos = $(this).scrollTop();
         if(scrpos > 80 ){
-            $("#hd").addClass('bg-dark border-bottom border-white').removeClass('bg-white')
+            $("#hd").addClass('scroll')
         }
         else{
-            $("#hd").removeClass('bg-dark border-bottom border-white').addClassClass('bg-white')
+            $("#hd").removeClass('scroll')
         }
           
         // $("body").attr("data-scrollps", $(this).scrollTop()) //내가 지어쓰는 속성은 data- 써서 만든다 여기서 scrollps는 변수가된다.
